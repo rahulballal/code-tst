0d30866009fcb8faef7143f6ef544ec9107ca252
Paritosh Mishra <paritosh149@outlook.com>
2021-11-16 10:31:49 +0000

    add definitions to empty functions

--- src/04_remote-data.js
+++ src/04_remote-data.js
@@ -1,3 +1,4 @@
+const got = require('got');
 /**
  *  This function fetches all todos from https://jsonplaceholder.typicode.com/todos
  *  and all users from https://jsonplaceholder.typicode.com/users
@@ -6,8 +7,25 @@
  *  and todos = [{ id: 2, userId: 1, completed: true}]
  *  Then the function would output [{ username: 'Tom', completed: 0}, { username: 'Jerry', completed: 1}]
  */
-function solution () {
+async function solution () {
+    try {
+		const todos = await got('https://jsonplaceholder.typicode.com/todos', {responseType: 'json'})
+        const completedTodos = [...todos.body].filter(todo => todo.completed);
 
+		const users =  await got('https://jsonplaceholder.typicode.com/users', {responseType: 'json'});
+
+        return [...users.body].map(user => {
+            return {username: user.username, completed: completedTodos.reduce((acc, current) => {
+                if(current.userId === user.id){
+                    return acc + 1
+                }
+                return acc
+            }, 0)
+        }
+        });
+	} catch (error) {
+		console.log('Error', error.response.body);
+	}
 }
 
 module.exports = solution
